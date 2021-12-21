1540da318d9bb0b6eb6c311ce430450a18529c12
Mayura-Zadane <zadane1778.litesh@gmail.com>
2021-12-22 01:15:11 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from docxtpl import DocxTemplate, InlineImage
+
+personName = ['Akash', 'Ankush', 'Aayushi', 'Mohit', 'Shlok', 'Aastha', 'Pooja']
+
+for pItr, p in enumerate(personName):
+    doc = DocxTemplate("inviteTemp.docx")
+    context = {
+        "todayStr": "21/12/2021",
+        "recipientName": p,
+        # "recipientName" : "John Doe",
+        "evntDtStr": "25st December, 2021",
+        "venueStr": "the beach",
+        "senderName": "Hayley Shah",
+        "bannering": InlineImage(doc, "party.jpg")
+    }
+    doc.render(context)
+    doc.save("invites/invitation_{0}.docx".format(pItr))
